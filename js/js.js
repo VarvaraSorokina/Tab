21fa7c454c4e7969d78b3686208581af6f6ee568
VarvaraSorokina <99732956+VarvaraSorokina@users.noreply.github.com>
2023-02-06 10:40:48 +0500

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,61 +1,61 @@
 window.addEventListener("DOMContentLoaded", () => {
     //tabs
-    const tabs = document.querySelectorAll(".tabheader__item"),
-        tabsContent = document.querySelectorAll(".tabcontent"),
-        tabsParent = document.querySelector(".tabheader");
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader');
 
     function hideTabContent() {
-        tabsContent.forEach((item) => {
-            item.style.display = "none";
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
         });
 
-        tabs.forEach((item) => {
-            item.classList.remove("tabheader__item_active");
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
         });
     }
-    function showhideTabContent(i = 0) {
-        tabsContent[i].style.display = "block";
-        tabs[i].classList.add("tabheader__item_active");
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active')
     }
     hideTabContent();
-    showhideTabContent();
+    showTabContent();
 
-    tabsParent.addEventListener("click", (event) => {
+    tabsParent.addEventListener('click', (event) => {
         const target = event.target;
-        if (target && target.classList.contains("tabheader__item")) {
+        if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
-                    showhideTabContent(i);
+                    showTabContent(i);
                 }
             });
         }
     });
 
     //timer
-    const timeEnd = "2023-03-01";
+    const timeEnd = '2023-02-03';
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60);
         return {
-            total: t,
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds,
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
         };
     }
     function setClock(selector, endtime) {
         const timer = document.querySelector(selector),
-            days = document.querySelector("#days"),
-            hours = document.querySelector("#hours"),
-            minutes = document.querySelector("#minutes"),
-            seconds = document.querySelector("#seconds");
-        timeInterval = setInterval(updateClock, 1000);
-        updateClock();
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        updateClock()
         function updateClock() {
             const t = getTimeRemaining(endtime);
             days.innerHTML = t.days;
@@ -67,7 +67,7 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         }
     }
-    setClock(".timer", timeEnd);
+    setClock('.timer', timeEnd);
 
     //modal
     function allModals() {
@@ -117,7 +117,7 @@ window.addEventListener("DOMContentLoaded", () => {
         total.textContent = `0${slides.length}`;
     } else {
         total.textContent = slides.length;
-    };
+    }
 
     function showSlides(n) {
         if (n > slides.length) {
@@ -133,11 +133,10 @@ window.addEventListener("DOMContentLoaded", () => {
         } else {
             current.textContent = slideIndex;
         }
-    };
-
+    }
     function plusSlides(n) {
         showSlides(slideIndex += n);
-    };
+    }
     prev.addEventListener('click', () => {
         plusSlides(-1);
     });
